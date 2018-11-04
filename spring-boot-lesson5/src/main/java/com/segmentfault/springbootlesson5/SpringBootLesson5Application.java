936f200c768714c58cd96de5c1ebd002a6b79c42
qiankeqin <qiankeqin@51xf.cn>
2018-11-04 14:59:21 +0800

    first commit

---
+++ spring-boot-lesson5/src/main/java/com/segmentfault/springbootlesson5/SpringBootLesson5Application.java
@@ -0,0 +1,12 @@
+package com.segmentfault.springbootlesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLesson5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLesson5Application.class, args);
+	}
+}
