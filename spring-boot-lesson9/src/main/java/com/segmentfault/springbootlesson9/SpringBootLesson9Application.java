9cbf8123dd75d8697bc9360e31f0455da8dfad06
qiankeqin <qiankeqin@51xf.cn>
2018-11-29 23:36:11 +0800

    springboot jpa

---
+++ spring-boot-lesson9/src/main/java/com/segmentfault/springbootlesson9/SpringBootLesson9Application.java
@@ -0,0 +1,12 @@
+package com.segmentfault.springbootlesson9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLesson9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLesson9Application.class, args);
+	}
+}
