9f09a4fc37cb4d732d2368cbece17eb1c8855c55
qiankeqin <qiankeqin@51xf.cn>
2018-12-07 13:33:22 +0800

    spring boot elasticsearch

---
+++ spring-boot-lessonx/src/main/java/com/segmentfault/springbootlessonx/SpringBootLessonxApplication.java
@@ -0,0 +1,12 @@
+package com.segmentfault.springbootlessonx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLessonxApplication.class, args);
+	}
+}
---
+++ spring-boot-lessonx/src/test/java/com/segmentfault/springbootlessonx/SpringBootLessonxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.segmentfault.springbootlessonx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLessonxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
