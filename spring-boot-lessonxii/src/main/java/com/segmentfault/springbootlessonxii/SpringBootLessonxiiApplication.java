909ed561e9bf2128e85877be21bb9e24720f9ce9
qiankeqin <qiankeqin@51xf.cn>
2018-12-17 13:51:15 +0800

    spring boot message

---
+++ spring-boot-lessonxii/src/main/java/com/segmentfault/springbootlessonxii/SpringBootLessonxiiApplication.java
@@ -0,0 +1,14 @@
+package com.segmentfault.springbootlessonxii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonxiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLessonxiiApplication.class, args);
+	}
+
+}
+
