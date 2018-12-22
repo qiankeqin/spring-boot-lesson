5990e60b62b9cb088d2e93f0f5ad8fc5a79872ff
qiankeqin <qiankeqin@51xf.cn>
2018-12-22 10:19:41 +0800

    spring boot kafka

---
+++ spring-boot-lessonxiii/src/test/java/com/segmentfault/springbootlessonxiii/SpringBootLessonxiiiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.segmentfault.springbootlessonxiii;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLessonxiiiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
