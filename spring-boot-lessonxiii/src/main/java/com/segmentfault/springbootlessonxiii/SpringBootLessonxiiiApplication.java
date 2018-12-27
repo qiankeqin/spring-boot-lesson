3aef6e0291e0e5f1c269f810737d62b787ff72ba
qiankeqin <qiankeqin@51xf.cn>
2018-12-27 13:25:54 +0800

    spring boot commons-validator

---
+++ spring-boot-lessonxiii/src/main/java/com/segmentfault/springbootlessonxiii/SpringBootLessonxiiiApplication.java
@@ -0,0 +1,14 @@
+package com.segmentfault.springbootlessonxiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonxiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLessonxiiiApplication.class, args);
+	}
+
+}
+
