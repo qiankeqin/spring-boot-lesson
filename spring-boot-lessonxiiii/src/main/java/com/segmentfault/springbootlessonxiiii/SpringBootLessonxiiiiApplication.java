1577d9b82397cfa14d7ae030b3a81319687b616b
qiankeqin <qiankeqin@51xf.cn>
2019-01-13 13:56:20 +0800

    前端页面

--- spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/SpringBootLessonxiiiiApplication.java
+++ spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/SpringBootLessonxiiiiApplication.java
@@ -2,12 +2,19 @@ package com.segmentfault.springbootlessonxiiii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class SpringBootLessonxiiiiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLessonxiiiiApplication.class, args);
+	}
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter(){
+		return new ServerEndpointExporter();
 	}
 
 }
--- spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/websocket/ChatRoomServletEndpoint.java
+++ spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/websocket/ChatRoomServletEndpoint.java
@@ -1,7 +1,10 @@
 package com.segmentfault.springbootlessonxiiii.websocket;
 
 
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 /**
  * @program: spring-boot-lesson
@@ -11,5 +14,45 @@ import javax.websocket.server.ServerEndpoint;
  **/
 @ServerEndpoint("/chat-room/{username}")
 public class ChatRoomServletEndpoint {
+
+    /**
+     * 打开Session时
+     * @param username
+     * @param session
+     */
+    @OnOpen
+    public void openSession(@PathParam("username") String username, Session session){
+        sendText("欢迎用户【"+username+"】登陆聊天室！",session);
+    }
+
+    /**
+     * 用户发送消息
+     * @param username
+     * @param session
+     * @param message
+     */
+    @OnMessage
+    public void onMessage(@PathParam("username") String username, Session session, String message){
+        sendText("用户【"+username+"】:"+message,session);
+    }
+
+    /**
+     * 关闭session时
+     * @param username
+     * @param session
+     */
+    @OnClose
+    public void closeSession(@PathParam("username") String username,Session session){
+        sendText("用户【"+username+"】退出聊天室，欢迎再来！",session);
+    }
+
+    private void sendText(String message, Session session){
+        RemoteEndpoint.Basic basicRemote = session.getBasicRemote();
+        try {
+            basicRemote.sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
