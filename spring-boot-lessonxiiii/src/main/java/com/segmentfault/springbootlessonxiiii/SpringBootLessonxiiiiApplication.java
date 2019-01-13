702232723273ad00dbaeb5bd54c9d21d76851abb
qiankeqin <qiankeqin@51xf.cn>
2019-01-13 09:31:30 +0800

    springboot websocket

---
+++ spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/SpringBootLessonxiiiiApplication.java
@@ -0,0 +1,14 @@
+package com.segmentfault.springbootlessonxiiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonxiiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLessonxiiiiApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-lessonxiiii/src/main/java/com/segmentfault/springbootlessonxiiii/websocket/ChatRoomServletEndpoint.java
@@ -0,0 +1,15 @@
+package com.segmentfault.springbootlessonxiiii.websocket;
+
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @program: spring-boot-lesson
+ * @description: 聊天室
+ * @author: qiankeqin
+ * @create: 2019-01-12 22:01
+ **/
+@ServerEndpoint("/chat-room/{username}")
+public class ChatRoomServletEndpoint {
+
+}
